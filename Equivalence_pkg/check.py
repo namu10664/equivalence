8b53410e8e60564afa7b46648719698373bb96df
namu10664 <119569307+namu10664@users.noreply.github.com>
2023-06-07 09:42:28 +0900

    Update check.py

--- Equivalence_pkg/check.py
+++ Equivalence_pkg/check.py
@@ -25,5 +25,5 @@ class Equivalence:
       return True
 
     @classmethod
-    def check_transitive(cls,R):
+    def check_equivalane(cls,R):
       return Equivalence.check_transitive(R) and Equivalence.check_symmetric(R) and Equivalence.check_reflexive(R)
